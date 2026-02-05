9f4549fcf2ccefd7a116bc8593204481469ade10
Eyad Elnagdy <116447411+EyadElnagdy@users.noreply.github.com>
2026-02-05 13:23:17 +0100

    Create views for gold layer in data warehouse

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,26 +1,47 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
 DROP VIEW IF EXISTS gold.dim_products;
 GO
 
 CREATE VIEW gold.dim_products
 AS
-	SELECT
-		ROW_NUMBER() OVER(ORDER BY a.prd_key, prd_start_dt) product_key,
-		a.prd_id product_id,
-		a.prd_key product_number,
-		a.prd_nm product_name,
-		a.cat_id,
-		a.prd_line product_line,
-		b.CAT category,
-		b.SUBCAT subcategory,
-		b.MAINTENANCE maintenance,
-		a.prd_cost cost,
-		a.prd_start_dt [start_date]
-	FROM silver.crm_prd_info a
-	LEFT JOIN silver.erp_PX_CAT_G1V2 b
-	ON a.cat_id=b.ID
-	where pd_end_dt is null;
+SELECT
+	ROW_NUMBER() OVER(ORDER BY a.prd_key, prd_start_dt) product_key,
+	a.prd_id product_id,
+	a.prd_key product_number,
+	a.prd_nm product_name,
+	a.cat_id,
+	a.prd_line product_line,
+	b.CAT category,
+	b.SUBCAT subcategory,
+	b.MAINTENANCE maintenance,
+	a.prd_cost cost,
+	a.prd_start_dt [start_date]
+FROM silver.crm_prd_info a
+LEFT JOIN silver.erp_PX_CAT_G1V2 b
+ON a.cat_id=b.ID
+where pd_end_dt is null;
 GO
-----------------------------------------------------------------
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
 DROP VIEW IF EXISTS gold.dim_customers;
 GO
 
@@ -46,8 +67,28 @@ LEFT JOIN silver.erp_loc_A101 b
 LEFT JOIN silver.erp_CUST_AZ12 c
 	ON a.cst_key=c.CID;
 GO
------------------------------------------------------------
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+DROP VIEW IF EXISTS gold.fact_sales;
+GO
 
+CREATE VIEW gold.fact_sales
+AS
+SELECT
+	a.sls_ord_num order_number,
+	b.product_key product_key,
+	c.customer_key customer_key,
+	a.sls_order_dt order_date,
+	a.sls_ship_dt shipping_date,
+	a.sls_due_dt due_date,
+	a.sls_sales sales_amount,
+	a.sls_quantity quantity,
+	a.sls_price price
+FROM silver.crm_sales_details a
+LEFT JOIN gold.dim_products b
+	ON a.sls_prd_key=b.product_number
+LEFT JOIN gold.dim_customers c
+	ON a.sls_cust_id=c.customer_id;
 
-
-
+GO
