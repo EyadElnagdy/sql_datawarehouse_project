24e5af8da8b8b040d306f71c772d60360d228365
Eyad Elnagdy <116447411+EyadElnagdy@users.noreply.github.com>
2026-02-05 12:17:14 +0100

    Create views for products and customers in gold schema

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,53 @@
+DROP VIEW IF EXISTS gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products
+AS
+	SELECT
+		ROW_NUMBER() OVER(ORDER BY a.prd_key, prd_start_dt) product_key,
+		a.prd_id product_id,
+		a.prd_key product_number,
+		a.prd_nm product_name,
+		a.cat_id,
+		a.prd_line product_line,
+		b.CAT category,
+		b.SUBCAT subcategory,
+		b.MAINTENANCE maintenance,
+		a.prd_cost cost,
+		a.prd_start_dt [start_date]
+	FROM silver.crm_prd_info a
+	LEFT JOIN silver.erp_PX_CAT_G1V2 b
+	ON a.cat_id=b.ID
+	where pd_end_dt is null;
+GO
+----------------------------------------------------------------
+DROP VIEW IF EXISTS gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers
+AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY a.cst_key,a.cst_create_date) customer_key,
+	a.cst_id customer_id,
+	a.cst_key customer_number,
+	a.cst_firstname first_name,
+	a.cst_lastname last_name,
+	a.cst_marital_status marital_status,
+	CASE
+		WHEN a.cst_gndr <> 'n/a' THEN a.cst_gndr
+		ELSE COALESCE(c.GEN,'n/a')
+	END AS gender,
+	CNTRY country,
+	BDATE birth_date,
+	a.cst_create_date create_date
+FROM silver.crm_cust_info a
+LEFT JOIN silver.erp_loc_A101 b
+	ON a.cst_key =b.CID
+LEFT JOIN silver.erp_CUST_AZ12 c
+	ON a.cst_key=c.CID;
+GO
+-----------------------------------------------------------
+
+
+
+
